7e84baf1c7a7103fa1f61e472d81b08b42abc453
KyunghyunKimRRC <kkim60@academic.rrc.ca>
2023-10-23 13:53:24 -0500

    Update Seed File
    Added Faker gem for seeding.
    Added population of Products in the seed.rb file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+Product.delete_all
+
+# Validation Test
+Product.create(
+    title: 'Test Product',
+    description: "Description of test product",
+    price: 12.34,
+    # stock_quantity: 50
+    )
+
+676.times do |i|
+    Product.find_or_create_by(
+        title: Faker::Commerce.product_name,
+        description: "Product #{i+1}",
+        price: Faker::Commerce.price,
+        stock_quantity: Faker::Number.digit
+    )
+end
