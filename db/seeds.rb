c3d9226272a312345679b2c171df2925472e8ad3
KyunghyunKimRRC <kkim60@academic.rrc.ca>
2023-10-23 14:41:58 -0500

    Update seeds.rb
    Changed population of products from faker to csv.
    Added population of categories from csv.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,40 @@
 require 'faker'
+require 'csv'
 
 Product.delete_all
+Category.delete_all
 
-# Validation Test
-Product.create(
-    title: 'Test Product',
-    description: "Description of test product",
-    price: 12.34,
-    # stock_quantity: 50
-    )
+# # Validation Test
+# Product.create(
+#     title: 'Test Product',
+#     description: "Description of test product",
+#     price: 12.34,
+#     # stock_quantity: 50
+#     )
 
-676.times do |i|
+# Populate using Faker
+# 676.times do |i|
+#     Product.find_or_create_by(
+#         title: Faker::Commerce.product_name,
+#         description: "Product #{i+1}",
+#         price: Faker::Commerce.price,
+#         stock_quantity: Faker::Number.digit
+#     )
+# end
+
+# Populate using CSV
+csv_file = Rails.root.join('db/products.csv')
+csv_data = File.read(csv_file)
+
+products = CSV.parse(csv_data, headers: true, encoding: 'iso-8859-1')
+
+products.each do |product|
+    category = Category.find_or_create_by(name: product['category'])
     Product.find_or_create_by(
-        title: Faker::Commerce.product_name,
-        description: "Product #{i+1}",
-        price: Faker::Commerce.price,
-        stock_quantity: Faker::Number.digit
+        title: product['name'],
+        description: product['description'],
+        price: product['price'],
+        stock_quantity: product['stock quantity'],
+        category: category
     )
 end
